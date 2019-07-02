c6f3de20c848cf120a73ef096f7f049c002d7ad0
naranjito72 <theclick36@gmail.com>
2019-07-02 14:21:14 +0200

    clase4 commit

--- src/app/customur/customerdata/customerdata.component.ts
+++ src/app/customur/customerdata/customerdata.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-
+import customers from '../../shared/data/data';
 
 @Component({
   selector: 'gc-customerdata',
@@ -14,13 +14,15 @@ import { ActivatedRoute } from '@angular/router';
 export class CustomerdataComponent implements OnInit {
 
 public identifier: any;
+public customres: Array<Object>;
 
 
   constructor(private route: ActivatedRoute) { }
+  this.selectedCustomer = customers.filter(e => {e.index === this.identifier; return e});
 
   ngOnInit() {
     this.route.params.subscribe(miParams => { this.identifier = miParams [ 'id' ]; });
-    
+
 /*OBSERVABLE, REUTILIZA LA MEMORIA CADA VEZ*/
 /* this.identifier = this.route.params['id'];*/
   }
--- src/app/customur/customerlist/customerlist.component.ts
+++ src/app/customur/customerlist/customerlist.component.ts
@@ -7,14 +7,13 @@ import Customer from 'src/app/shared/interfaces/customer.interface';
   styleUrls: ['./customerlist.component.scss']
 })
 export class CustomerlistComponent implements OnInit {
-  public customers: Customer;
-  public  identifier: any;
-   id = 0;
+  customers: Array<Object>;
 
 
   constructor() { }
 
   ngOnInit() {
+
   }
 
 }
