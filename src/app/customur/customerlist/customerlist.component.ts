09d2021b15082f981f40e563d7c2b5ccd6703ee3
naranjito72 <theclick36@gmail.com>
2019-07-04 12:45:07 +0200

    12:45 commit

--- src/app/customur/customerlist/customerlist.component.ts
+++ src/app/customur/customerlist/customerlist.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import Customer from 'src/app/shared/interfaces/customer.interface';
+
 
 @Component({
   selector: 'gc-customerlist',
@@ -7,7 +7,6 @@ import Customer from 'src/app/shared/interfaces/customer.interface';
   styleUrls: ['./customerlist.component.scss']
 })
 export class CustomerlistComponent implements OnInit {
-  customers: Array<Object>;
 
 
   constructor() { }
